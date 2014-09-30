5723956fd6b4b8bd026528d7f546938351bd42f3
Dennis Benkert <spinecrasher@googlemail.com>
2014-09-30 10:56:54 +0000

    added fixtures loading to php-fpm service

--- php-fpm/start.sh
+++ php-fpm/start.sh
@@ -6,5 +6,9 @@ echo "env[APP_SERVER_NAME] = ${APP_SERVER_NAME}" >> /etc/php5/fpm/pool.d/www.con
 echo "env[SYMFONY__REDIS_PORT] = ${REDIS_1_PORT_6379_TCP_PORT}" >> /etc/php5/fpm/pool.d/www.conf
 echo "env[SYMFONY__REDIS_ADDRESS] = ${REDIS_1_PORT_6379_TCP_ADDR}" >> /etc/php5/fpm/pool.d/www.conf
 
+/bin/bash -l -c "php app/console redis:flushall --no-interaction"
+/bin/bash -l -c "php app/console giantswarm:temperature:add cologne 20"
+/bin/bash -l -c "php app/console giantswarm:temperature:add cologne 22"
+/bin/bash -l -c "php app/console giantswarm:temperature:add cologne 21"
+
 exec /usr/sbin/php5-fpm --nodaemonize
-#/bin/bash
