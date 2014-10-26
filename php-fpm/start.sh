956a84e7a92aaa6041e4180a1295686433c3310e
Dennis Benkert <spinecrasher@googlemail.com>
2014-10-26 11:45:06 +0100

    changed environment variable names to the ones docker would create for linking containers

--- php-fpm/start.sh
+++ php-fpm/start.sh
@@ -3,8 +3,8 @@
 sed -i "s@listen = /var/run/php5-fpm.sock@listen = 9000@" /etc/php5/fpm/pool.d/www.conf
 
 echo "env[APP_SERVER_NAME] = ${APP_SERVER_NAME}" >> /etc/php5/fpm/pool.d/www.conf
-echo "env[SYMFONY__REDIS_PORT] = ${REDIS_1_PORT_6379_TCP_PORT}" >> /etc/php5/fpm/pool.d/www.conf
-echo "env[SYMFONY__REDIS_ADDRESS] = ${REDIS_1_PORT_6379_TCP_ADDR}" >> /etc/php5/fpm/pool.d/www.conf
+echo "env[SYMFONY__REDIS_PORT] = ${REDIS_PORT_6379_TCP_PORT}" >> /etc/php5/fpm/pool.d/www.conf
+echo "env[SYMFONY__REDIS_ADDRESS] = ${REDIS_PORT_6379_TCP_ADDR}" >> /etc/php5/fpm/pool.d/www.conf
 
 /bin/bash -l -c "php app/console redis:flushall --no-interaction"
 /bin/bash -l -c "php app/console giantswarm:temperature:add cologne 20"
