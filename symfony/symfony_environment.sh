05724cd5df7bcb34f6c97f97ce4a030513ff0478
timglabisch <code@tim.ainfach.de>
2014-11-11 15:31:01 +0100

    ....

--- symfony/symfony_environment.sh
+++ symfony/symfony_environment.sh
@@ -3,6 +3,7 @@ export SYMFONY__REDIS_ADDRESS=${REDIS_PORT_6379_TCP_ADDR}
 
 export SYMFONY__database_host=${MYSQL_PORT_3306_TCP_ADDR}
 export SYMFONY__database_port=${MYSQL_PORT_3306_TCP_PORT}
+export SYMFONY__database_password=root
 
 export SYMFONY__elasticsearch_host=${ELASTICSEARCH_PORT_9200_TCP_ADDR}
 export SYMFONY__elasticsearch_port=${ELASTICSEARCH_PORT_9200_TCP_PORT}
