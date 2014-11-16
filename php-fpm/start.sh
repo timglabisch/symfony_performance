0ec4e2c2f063678631bea5521f665729c09a4be8
timglabisch <code@tim.ainfach.de>
2014-11-16 13:12:17 +0100

    mongo, kafa + postgres

--- php-fpm/start.sh
+++ php-fpm/start.sh
@@ -17,6 +17,16 @@ echo "env[SYMFONY__database_port] = ${MYSQL_PORT_3306_TCP_PORT}" >> /etc/php5/fp
 echo "env[SYMFONY__elasticsearch_host] = ${ELASTICSEARCH_PORT_9200_TCP_ADDR}" >> /etc/php5/fpm/pool.d/www.conf
 echo "env[SYMFONY__elasticsearch_port] = ${ELASTICSEARCH_PORT_9200_TCP_PORT}" >> /etc/php5/fpm/pool.d/www.conf
 
+echo "env[SYMFONY__mongodb_host] = ${MONGODB_PORT_27017_TCP_ADDR}" >> /etc/php5/fpm/pool.d/www.conf
+echo "env[SYMFONY__mongodb_port] = ${MONGODB_PORT_27017_TCP_PORT}" >> /etc/php5/fpm/pool.d/www.conf
+
+echo "env[SYMFONY__postgres_host] = ${POSTGRES_PORT_5432_TCP_ADDR}" >> /etc/php5/fpm/pool.d/www.conf
+echo "env[SYMFONY__postgres_port] = ${POSTGRES_PORT_5432_TCP_PORT}" >> /etc/php5/fpm/pool.d/www.conf
+
+echo "env[SYMFONY__kafka_host] = ${KAFKA_PORT_9092_TCP_ADDR}" >> /etc/php5/fpm/pool.d/www.conf
+echo "env[SYMFONY__kafka_port] = ${KAFKA_PORT_9092_TCP_PORT}" >> /etc/php5/fpm/pool.d/www.conf
+
+
 if [ "$*" == "/bin/bash" ]; then
     exec /usr/sbin/php5-fpm --nodaemonize
 else
