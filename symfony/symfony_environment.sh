4b397fa678114e01dbbe2ea5e980bf6afdfdb1db
timglabisch <code@tim.ainfach.de>
2014-11-16 20:16:02 +0100

    ...

--- symfony/symfony_environment.sh
+++ symfony/symfony_environment.sh
@@ -7,3 +7,15 @@ export SYMFONY__database_password=root
 
 export SYMFONY__elasticsearch_host=${ELASTICSEARCH_PORT_9200_TCP_ADDR}
 export SYMFONY__elasticsearch_port=${ELASTICSEARCH_PORT_9200_TCP_PORT}
+
+export SYMFONY__mongodb_host=${MONGODB_PORT_27017_TCP_ADDR}
+export SYMFONY__mongodb_port=${MONGODB_PORT_27017_TCP_PORT}
+
+export SYMFONY__postgres_host=${POSTGRES_PORT_5432_TCP_ADDR}
+export SYMFONY__postgres_port=${POSTGRES_PORT_5432_TCP_PORT}
+
+export SYMFONY__kafka_host=${KAFKA_PORT_9092_TCP_ADDR}
+export SYMFONY__kafka_port=${KAFKA_PORT_9092_TCP_PORT}
+
+export SYMFONY__silex_host=${SILEX_PORT_5560_TCP_ADDR}
+export SYMFONY__silex_port=${SILEX_PORT_5560_TCP_PORT}
