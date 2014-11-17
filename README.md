e0d066aefb7998d785b5c8733a9ab3538f4dc046
timglabisch <code@tim.ainfach.de>
2014-11-17 23:01:04 +0100

    ...

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
-#Dockerized Symfony Project
+# Symfony Docker
 
-A symfony project utilizing Docker based on PHP-FPM and nginx.
+sets up a new
 
 ## Running
 
 You can run the Docker environment using [fig](http://www.fig.sh/):
 
-    $ fig up -d
-
-You can run one-shot command inside the `symfony` service container:
-
-    $ fig run --rm symfony composer install
-    $ fig run --rm symfony php app/console cache:clear
-
-## Example Application
-
-The example app implements a simple weather application using Redis.
-
-During the startup of the `fpm` service some fixture data will be generated for
-you automatically. If you want to add your own entries you can use the console
-command shipped with the example application:
-
-    $fig run --rm symfony php app/console giantswarm:temperature:add cologne 30
+    curl -L https://raw.githubusercontent.com/timglabisch/symfony-docker-example/master/symfony.sh | sh
---
+++ symfony.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function_exists() {
+    which $1 > /dev/null
+    return $?
+}
+
+echo -e " \e[32m
+
+  mmmm                  m''                                     #
+ #'   ' m   m  mmmmm  mm#mm   mmm   m mm   m   m          mmm   # mm
+ '#mmm  'm m'  # # #    #    #' '#  #'  #  'm m'         #   '  #'  #
+     '#  #m#   # # #    #    #   #  #   #   #m#           '''m  #   #
+ 'mmm#'  '#    # # #    #    '#m#'  #   #   '#      #    'mmm'  #   #
+         m'                                 m'
+        ''                                 ''
+"
+
+echo -e "\e[39m"
+
+if ! function_exists git; then
+    echo ""
+    echo "install git first, using"
+    echo -e "\e[31m$ sudo apt-get install -y git"
+    echo ""
+    exit
+fi
+
+if ! function_exists docker; then
+    echo ""
+    echo "install docker first, using"
+    echo -e "\e[31m$ sudo apt-get install -y docker"
+    echo ""
+    exit
+fi
+
+if ! function_exists fig; then
+    echo ""
+    echo "install fig first, using"
+    echo -e "\e[31m$ sudo apt-get install -y python-pip && sudo pip install fig"
+    echo ""
+    exit
+fi
+
+DIR="./"
+if [ "$1" != "" ]; then
+    DIR=$1
+    mkdir -p $DIR && cd $DIR
+fi
+
+git clone https://github.com/timglabisch/symfony-docker-example ./ &&
+sudo fig build &&
+sudo fig run --rm symfony composer install --prefer-source --no-interaction &&
+sudo chmod -R 777 symfony/code/app/cache &&
+sudo chmod -R 777 symfony/code/app/logs &&
+sudo fig run --rm symfony app/console generate:bundle &&
+sudo chmod -R 777 symfony/code/app/cache &&
+sudo chmod -R 777 symfony/code/app/logs &&
+sudo fig up
